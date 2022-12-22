17378d37d2cd97ee6b102e192a8e4573dd688303
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2022-12-22 21:11:28 +0000

    I corrected Jsp servlet with name remove book and now we can remove to use methot remove book from main java class DataBase Facade

--- JavaFull/src/main/java/ua/com/foxminded/db/DatabaseFacade.java
+++ JavaFull/src/main/java/ua/com/foxminded/db/DatabaseFacade.java
@@ -8,134 +8,81 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class DatabaseFacade {
-	
-	
-	
 
-	
-	
 	public static List<String> getBooks() {
-		
-		 
-		
+
 		List<String> result = new ArrayList<String>();
 
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books", "postgres",
+				"1234")) {
 
-		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books",
-				"postgres", "1234")) {
-			
 			System.out.println("Java JDBC PostgreSQL Example");
 
 			System.out.println("Connected to PostgreSQL database!");
-			
-			
-			
+
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM books.books");
-			
-			
-			
+
 			while (resultSet.next()) {
 				result.add(resultSet.getString("name"));
-				//result.add(resultSet.getString("ID"));
+				// result.add(resultSet.getString("ID"));
 			}
-		} 
-		
+		}
+
 		catch (SQLException e) {
 			System.out.println("Connection failure.");
 			e.printStackTrace();
 		}
-		
-		
+
 		return result;
 	}
-	
-	
-	
-	
 
-	
-	 public static void addBook (String newBook){
-		
-		 try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books",
-					"postgres", "1234")) {
-				
-				Statement statement = connection.createStatement();
-				statement.executeUpdate("INSERT INTO books.books (\"NAME\") VALUES ( '" + newBook + "'  )");
-				statement.close();
-				connection.close();}	
-		 
-			
+	public static void addBook(String newBook) {
 
-		
-		catch (SQLException e) {
-			System.out.println("Connection failure.");
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
-	
-	public static List<String> removeBook (String removeBook){
-		
-		List<String> result = new ArrayList<String>();
-		
-		
-		
-		
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books", "postgres",
+				"1234")) {
 
-		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books",
-				"postgres", "1234")) {
-			System.out.println("Java JDBC PostgreSQL Example");
-
-			System.out.println("Connected to PostgreSQL database!");
 			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM books.books");
-			
-			
-			while (resultSet.next()) {
-				result.add(resultSet.getString("name"));
-				//result.add(resultSet.getString("ID"));
-			}
-		} 
-		
+			statement.executeUpdate("INSERT INTO books.books (\"NAME\") VALUES ( '" + newBook + "'  )");
+			statement.close();
+			connection.close();
+		}
+
 		catch (SQLException e) {
 			System.out.println("Connection failure.");
 			e.printStackTrace();
 		}
-		
-		
-		
-		return result;
-		
-		
-		
-	}
-	
-	
-	/* public static List<String> addBooks() {
-	        List<String> result=new ArrayList<String>();
-	        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books",
-					"postgres", "1234")){
-	            Scanner scanner = new Scanner(System.in);
-	            while (scanner.hasNextLine()) {
-	                result.addAll(addBooks());
-	            }
-	            scanner.close();
-	        } catch (SQLException e) {
-			
-				e.printStackTrace();
-			} 
-	        return result;
-	    } */
-	 
-	
-	
 
-	
-	
+	}
+
+	public static void removeBook(String oldBook) {
+
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books", "postgres",
+				"1234")) {
+
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("DELETE FROM books.books WHERE (\"NAME\") = '" + oldBook + "'");
+			statement.close();
+			connection.close();
+		}
+
+		catch (SQLException e) {
+			System.out.println("Connection failure.");
+			e.printStackTrace();
+		}
+
+	}
+
 }
+
+/*
+ * public static List<String> addBooks() { List<String> result=new
+ * ArrayList<String>(); try (Connection connection =
+ * DriverManager.getConnection("jdbc:postgresql://localhost:5432/Books",
+ * "postgres", "1234")){ Scanner scanner = new Scanner(System.in); while
+ * (scanner.hasNextLine()) { result.addAll(addBooks()); } scanner.close(); }
+ * catch (SQLException e) {
+ * 
+ * e.printStackTrace(); } return result; }
+ */
